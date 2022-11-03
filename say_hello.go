287a6c08e9a4f5b2e6e481ec3b0a72053c6f4821
stephaniemartin <martin.lilik.a@gmail.com>
2022-11-03 10:54:30 +0700

    update with parameter name

--- say_hello.go
+++ say_hello.go
@@ -1,10 +1,11 @@
 package go_say_hello
 
-func SayHello() string {
-	return "hello world"
+func SayHello(name string) string {
+	return "hello " + name
 }
 
 /**
+update module:
 git tag vx.x.x
 git push origin vx.x.x
 */
